bf346833687cfb34dc696a3dd9decb631badea75
kmtsui <a122014@hotmail.com>
2021-10-02 23:23:01 +0100

    Create optical_fit.entrypoint.sh

---
+++ optical_fit.entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+source /opt/geant4/bin/geant4.sh
+export WCSIMDIR=/WCSim
+export LD_LIBRARY_PATH=$WCSIMDIR/:$LD_LIBRARY_PATH
+source /optical_fit/build/Linux/setup.sh
+
+exec "$@"
